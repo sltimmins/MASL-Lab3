3d0aeea78a84353863361ea93e1b5228a2a1dc51
Caleb Moore <ctmoore@smu.edu>
2022-10-18 18:26:23 -0500

    modelo plinko board

--- Commotion/GameScene.swift
+++ Commotion/GameScene.swift
@@ -32,7 +32,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     // MARK: View Hierarchy Functions
-    let spinBlock = SKSpriteNode()
+//    let spinBlock = SKSpriteNode()
     let scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
     var score:Int = 0 {
         willSet(newValue){
@@ -52,12 +52,42 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // make sides to the screen
         self.addSidesAndTop()
         
-        // add some stationary blocks
-        self.addStaticBlockAtPoint(CGPoint(x: size.width * 0.1, y: size.height * 0.25))
-        self.addStaticBlockAtPoint(CGPoint(x: size.width * 0.9, y: size.height * 0.25))
+        // add some stationary modelos
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.1, y: size.height * 0.85))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.366, y: size.height * 0.85))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.633, y: size.height * 0.85))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.9, y: size.height * 0.85))
+        
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.233, y: size.height * 0.75))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.5, y: size.height * 0.75))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.766, y: size.height * 0.75))
+
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.1, y: size.height * 0.65))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.366, y: size.height * 0.65))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.633, y: size.height * 0.65))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.9, y: size.height * 0.65))
+        
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.233, y: size.height * 0.55))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.5, y: size.height * 0.55))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.766, y: size.height * 0.55))
+        
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.1, y: size.height * 0.45))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.366, y: size.height * 0.45))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.633, y: size.height * 0.45))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.9, y: size.height * 0.45))
+        
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.233, y: size.height * 0.35))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.5, y: size.height * 0.35))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.766, y: size.height * 0.35))
+        
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.1, y: size.height * 0.25))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.366, y: size.height * 0.25))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.633, y: size.height * 0.25))
+        self.addStaticPegAtPoint(CGPoint(x: size.width * 0.9, y: size.height * 0.25))
+        
         
         // add a spinning block
-        self.addBlockAtPoint(CGPoint(x: size.width * 0.5, y: size.height * 0.35))
+//        self.addBlockAtPoint(CGPoint(x: size.width * 0.5, y: size.height * 0.35))
         
         self.addSpriteBottle()
         
@@ -81,7 +111,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func addSpriteBottle(){
         let spriteA = SKSpriteNode(imageNamed: "sprite") // this is literally a sprite bottle... 😎
         
-        spriteA.size = CGSize(width:size.width*0.1,height:size.height * 0.1)
+        spriteA.size = CGSize(width:size.width*0.15,height:size.height * 0.08)
         
         let randNumber = random(min: CGFloat(0.1), max: CGFloat(0.9))
         spriteA.position = CGPoint(x: size.width * randNumber, y: size.height * 0.75)
@@ -96,22 +126,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(spriteA)
     }
     
-    func addBlockAtPoint(_ point:CGPoint){
-        
-        spinBlock.color = UIColor.red
-        spinBlock.size = CGSize(width:size.width*0.15,height:size.height * 0.05)
-        spinBlock.position = point
-        
-        spinBlock.physicsBody = SKPhysicsBody(rectangleOf:spinBlock.size)
-        spinBlock.physicsBody?.contactTestBitMask = 0x00000001
-        spinBlock.physicsBody?.collisionBitMask = 0x00000001
-        spinBlock.physicsBody?.categoryBitMask = 0x00000001
-        spinBlock.physicsBody?.isDynamic = true
-        spinBlock.physicsBody?.pinned = true
-        
-        self.addChild(spinBlock)
-
-    }
+//    func addBlockAtPoint(_ point:CGPoint){
+//
+//        spinBlock.color = UIColor.red
+//        spinBlock.size = CGSize(width:size.width*0.15,height:size.height * 0.05)
+//        spinBlock.position = point
+//
+//        spinBlock.physicsBody = SKPhysicsBody(rectangleOf:spinBlock.size)
+//        spinBlock.physicsBody?.contactTestBitMask = 0x00000001
+//        spinBlock.physicsBody?.collisionBitMask = 0x00000001
+//        spinBlock.physicsBody?.categoryBitMask = 0x00000001
+//        spinBlock.physicsBody?.isDynamic = true
+//        spinBlock.physicsBody?.pinned = true
+//
+//        self.addChild(spinBlock)
+//
+//    }
     
     func addStaticBlockAtPoint(_ point:CGPoint){
         let 🔲 = SKSpriteNode()
@@ -126,6 +156,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         🔲.physicsBody?.allowsRotation = true
         
         self.addChild(🔲)
+        
+    }
+    
+    func addStaticPegAtPoint(_ point:CGPoint){
+        let modelo = SKSpriteNode(imageNamed: "modelo")
+        
+//        modelo.color = UIColor.init(red: 210/255, green: 180/255, blue: 140/255, alpha: 1)
+        modelo.size = CGSize(width:size.width*0.07,height:size.height * 0.035)
+        modelo.position = point
+        
+        modelo.physicsBody = SKPhysicsBody(circleOfRadius:modelo.size.height/3)
+        modelo.physicsBody?.isDynamic = true
+        modelo.physicsBody?.pinned = true
+        modelo.physicsBody?.allowsRotation = false
+        
+        self.addChild(modelo)
         
     }
     
@@ -144,7 +190,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         top.position = CGPoint(x:size.width*0.5, y:size.height)
         
         for obj in [left,right,top]{
-            obj.color = UIColor.red
+            obj.color = UIColor.init(red: 210/255, green: 180/255, blue: 140/255, alpha: 1)
             obj.physicsBody = SKPhysicsBody(rectangleOf:obj.size)
             obj.physicsBody?.isDynamic = true
             obj.physicsBody?.pinned = true
@@ -159,9 +205,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
-        if contact.bodyA.node == spinBlock || contact.bodyB.node == spinBlock {
-            self.score += 1
-        }
+//        if contact.bodyA.node == spinBlock || contact.bodyB.node == spinBlock {
+//            self.score += 1
+//        }
     }
     
     // MARK: Utility Functions (thanks ray wenderlich!)
